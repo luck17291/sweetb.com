04a8ea78166b4500220e1aa53a5136d0b37587b0
unknown <Emerric@.(none)>
2013-09-17 00:25:28 +0700

    design view

--- application/views/main.php
+++ application/views/main.php
@@ -1,5 +1,16 @@
 <!DOCTYPE html>
 <html>
-	<head></head>
-	<body>this is main page sweetb.com</body>
+	<head>
+		<link rel="stylesheet" type="text/css" href="/sweetb.com/assests/CSS/style.css" />
+		
+	</head>
+	<body>
+		<header id="header">
+			<?php $this->load->view("top");?>
+		</header>
+		
+		<div class="line_header"></div>
+		<div id="body">this is body</div>
+		<footer id="footer">this is footer</footer>
+	</body>
 </html>
---
+++ application/views/top.php
@@ -0,0 +1,17 @@
+<hgroup>
+	<h1>SweetB.com</h1>
+	<h2>Best Gallery Gay</h2>
+</hgroup>
+<nav>
+	<ul class="main-nav">
+		<li>Home</li>
+		<li>My Gallery</li>
+		<li>Anime</li>
+		<li>Boys</li>
+		<li>Top 20</li>
+		<li>Other</li>
+	</ul>
+</nav>
+<div class="header-banner">
+	<a href="/sweetb.com/assests/images/banner.png"></a>
+</div>
