97097eab04a11299787cab1322bd0a46071e0d55
luck17291 <luck17291@yahoo.com>
2013-09-23 15:44:53 +0700

    design view

---
+++ application/views/main.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="/sweetb.com/assests/CSS/style.css" />
+		
+	</head>
+	<body>
+		<header id="header">
+			<?php $this->load->view("top");?>
+		</header>
+		<div id="middle">this is body!</div>
+		<footer id="footer">this is footer</footer>
+	</body>
+</html>
---
+++ application/views/top.php
@@ -0,0 +1,23 @@
+<div class="top-banner">
+	<div>
+		<hgroup>
+			<h1>SweetB.com</h1>
+			<h2>Best Gallery Gay</h2>
+		</hgroup>
+	</div>
+	<div class="header-banner">
+		<img src="/sweetb.com/assests/images/banner.png"/>
+	</div>
+</div>
+<dir>
+	<nav>
+		<ul class="main-nav">
+			<li>Home</li>
+			<li>My Gallery</li>
+			<li>Anime</li>
+			<li>Boys</li>
+			<li>Top 20</li>
+			<li>Other</li>
+		</ul>
+	</nav>
+</dir>
